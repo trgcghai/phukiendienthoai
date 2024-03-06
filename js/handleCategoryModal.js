054caea605f7f9abf0f1671aa2a14ff1928ccfac
trgcghai <conghai.tpma@gmail.com>
2024-03-06 14:17:31 +0700

    fix header

--- js/handleCategoryModal.js
+++ js/handleCategoryModal.js
@@ -1,7 +1,15 @@
 const showCategoriesModal = document.querySelector(".categories");
+const showCollapseCategories = document.querySelector('.collapse_categories')
 const backDrop = document.querySelector(".backdrop");
 const categories = document.querySelector(".categories_list_container");
 const categoriesItems = document.querySelectorAll(".categories_item");
+
+showCollapseCategories.addEventListener('click' , () => {
+  window.scrollTo(0, 0);
+  document.querySelector('.toggle_list').style.display = 'none'
+  backDrop.style.display = "block";
+  categories.style.zIndex = 2;
+})
 
 showCategoriesModal.addEventListener("click", () => {
   window.scrollTo(0, 0);
