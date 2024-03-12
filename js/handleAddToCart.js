bbde3f6aade1114269c34435d43ae0e0dcfcea15
trgcghai <conghai.tpma@gmail.com>
2024-03-12 20:32:22 +0700

    add toast add product to card / add cart.html / doing cart logic

---
+++ js/handleAddToCart.js
@@ -0,0 +1,33 @@
+const addToCartBtn = document.querySelector(".addToCart_btn")
+const productQuantities = document.getElementById('quantity_input')
+
+addToCartBtn.addEventListener('click', () => {
+    const currProduct = {
+        productId: localStorage.getItem('productId'),
+        quantity: productQuantities.value
+    }
+    
+    let cart = localStorage.getItem('cart')
+    if (cart === null) {
+        cart = []
+    } else {
+        cart = JSON.parse(cart)
+    }
+
+    if (cart.filter(item => item.productId == currProduct.productId).length) {
+        cart.forEach(item => {
+            if (item.productId == currProduct.productId) {
+                item.quantity = (parseInt(item.quantity) + parseInt(currProduct.quantity)).toString()
+            }
+        });   
+    } else {
+        cart.push(currProduct)
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart))
+
+    productQuantities.value = 1
+})
+
+
+
